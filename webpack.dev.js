3f1ca479d7701056f89070ca27d981afea2dff89
Jenia <jeka.lyah1992@@gmail.com>
2021-09-20 15:32:00 +0300

    added countdown to midnight

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,14 +1,23 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
+const fs = require("fs");
 
 module.exports = merge(common, {
   //...
   devServer: {
     client: {
+      https: true,
       overlay: {
         errors: false,
         warnings: false,
+      },
+      https: {
+        key: fs.readFileSync("./localhost-key.pem"),
+        cert: fs.readFileSync("./localhost.pem"),
+        ca: fs.readFileSync(
+          "/Users/jeka/Library/Application Support/mkcert/rootCA-key.pem"
+        ),
       },
     },
   },
